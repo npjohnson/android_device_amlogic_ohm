0ffeceb84610a9abed293dabf4d87c2e5f399111
Vitaliy Tomin <highwaystar.ru@gmail.com>
2026-07-30 18:35:31 +0800

    kvim1s: Set BT and Wifi firmwares as proprietary-files
    * fix firmware loading
    
    Change-Id: Iab21c214ab705ba15fc5b28de8118cff6335e337

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ $(call soong_config_set,brcm_libbt,bdroid_buildcfg_include_dir,$(LOCAL_PATH)/blu
 $(call soong_config_set,brcm_libbt,custom_bt_config,//$(LOCAL_PATH):vnd_kvim1s.txt)
 
 ## Bluetooth firmware
-include kernel/amlogic/kernel-modules/dhd-driver/firmware/bluetooth/bluetooth.mk
+#include kernel/amlogic/kernel-modules/dhd-driver/firmware/bluetooth/bluetooth.mk
 
 ## Factory
 PRODUCT_HOST_PACKAGES += \
@@ -31,7 +31,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/broadcom/libbt
 
 ## Wi-Fi firmware
-include kernel/amlogic/kernel-modules/dhd-driver/firmware/wifi/wifi.mk
+#include kernel/amlogic/kernel-modules/dhd-driver/firmware/wifi/wifi.mk
 
 ## Inherit from the common tree product makefile
 $(call inherit-product, device/amlogic/ne-common/ne.mk)
